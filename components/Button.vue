63b4c72d3d37cfc4ab6de87de18736cb7b01b9cf
yuuaHP <identity@yuua.dev>
2024-08-07 01:44:44 +0900

    fix: button display issue on webkit

--- components/Button.vue
+++ components/Button.vue
@@ -9,11 +9,11 @@ const props = defineProps<{
   <div :data-dark="dark" class="
     px-4 py-1
     font-bold cursor-pointer inline-block rounded-xl select-none
-    bg-white h-fit
+    bg-white
     data-[dark=true]:bg-brown-depth data-[dark=true]:text-brown-background
     hover:bg-brown-primary hover:text-brown-background
     data-[dark=true]:hover:bg-brown-background data-[dark=true]:hover:text-brown-primary
-    transition-all">
+    transition-all text-nowrap">
     <font-awesome-icon :icon="['fad', icon]" class="mr-1" />
     <slot/>
   </div>
