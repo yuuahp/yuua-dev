bd6cc41d7594f8d73893cf95df91dad1c1532f32
yuua <identity@yuua.dev>
2024-08-07 23:23:51 +0900

    fix: locale header link

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-
+const localePath = useLocalePath()
 </script>
 
 <template>
   <div class="group w-full h-8 hover:h-12 glass sticky top-0 z-50 border-b border-brown-background text-brown-secondary hover:text-brown-primary flex items-center justify-between px-3 hover:px-4 transition-all">
-    <a href="/" class="hidden group-hover:inline-block">
+    <NuxtLink :to="localePath('/')" class="hidden group-hover:inline-block">
       <img src="~/assets/yuua-logo-light.svg" alt="yuua's logo" class="h-6"/>
-    </a>
-    <a href="/" class="inline-block group-hover:hidden h-2 w-16 bg-brown-secondary rounded-full"></a>
+    </NuxtLink>
+    <NuxtLink :to="localePath('/')" class="inline-block group-hover:hidden h-2 w-16 bg-brown-secondary rounded-full"></NuxtLink>
     <slot/>
   </div>
 </template>
